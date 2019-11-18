1f4c99e07d543e174479e2d31162873211cbf7c1
Keven <keven.santos.sz@gmail.com>
2019-11-18 15:00:17 -0300

    added build script

---
+++ scripts/build.pkg.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+echo "Deleting Dist Folder and generating new dist folder"
+rimraf ./dist && tsc
+
+echo "Copying ormconfig and .env files to /dist"
+cp ormconfig.json .env dist/
+
+echo "Build finished"
